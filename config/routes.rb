51df15401bda2c23b3d31e789b201d0b4e2cad6b
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2018-04-18 15:27:52 -0500

    create route PATCH /products/:id

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ get '/products/new', to: 'products#new', as: 'new_product'
 post '/products', to: 'products#create'
 get '/products/:id', to: 'products#show', as: 'product'
 get '/products/:id/edit', to: 'products#edit'
-
+patch '/products/:id', to: 'products#update'
 end
