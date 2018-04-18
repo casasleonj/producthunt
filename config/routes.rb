c40be8e934153705d5a03af26aefcbbc64c2dc82
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2018-04-18 15:40:05 -0500

    agregar enlace ditar y show en name

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ get '/products', to: 'products#index'
 get '/products/new', to: 'products#new', as: 'new_product'
 post '/products', to: 'products#create'
 get '/products/:id', to: 'products#show', as: 'product'
-get '/products/:id/edit', to: 'products#edit'
+get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
 patch '/products/:id', to: 'products#update'
 end
