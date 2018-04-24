2d54f7eff3d923f75c603fc1b2db6e8d93272bdd
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2018-04-24 11:37:16 -0500

    change gem file version of paperclip and aws s3

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'gravatar-ultimate'
 gem 'annotate'
 gem "paperclip"
 #aws s3
-gem 'aws-sdk-s3', '~> 1.0.0.rc2'
+gem 'aws-sdk-s3'
 #Appliacion javascript
 gem 'jquery-rails'
 # Use ActiveStorage variant
--- app/models/product.rb
+++ app/models/product.rb
@@ -19,12 +19,13 @@ class Product < ActiveRecord::Base
 belongs_to :user
 has_many :comments, dependent: :destroy
 has_many :votes, dependent: :destroy
-
+# This method associates the attribute ":avatar" with a file attachment
 has_attached_file :image, styles: {medium: '300x300>', square: '200x200#', thumb: '100x100>'}
 
 
 	validates :name, presence: true
 	validates :url, presence: true
+	# Validate the attached image is image/jpg, image/png, etc
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 def voted_by?(user)
